48c6a7f3efd7e728373261ef5bdd38cb5e32e1d1
Luan Missel <luan.missel.lourenco@gmail.com>
2026-05-14 21:09:06 -0300

    atualiza o código de controller e corrige redundâncias

--- src/main/java/br/com/luanmissel/todolist/task/ITaskRepository.java
+++ src/main/java/br/com/luanmissel/todolist/task/ITaskRepository.java
@@ -3,8 +3,9 @@ package br.com.luanmissel.todolist.task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 @Repository
 public interface ITaskRepository extends JpaRepository <TaskModel, UUID> {
-
+    List<TaskModel> findByUserId(UUID userId);
 }
